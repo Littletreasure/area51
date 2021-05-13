f190168433866fad76846a3e182fa1992990387b
Littletreasure <ruth_sargent@yahoo.co.uk>
2021-05-13 22:32:29 +0100

    add filler text and styling

--- src/components/About.js
+++ src/components/About.js
@@ -5,9 +5,43 @@ class About extends Component {
     return (
       <div className="about">
         <h2>About</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed
+          ornare velit, in porta metus. Curabitur vel facilisis diam. Ut
+          tincidunt sodales nunc, rutrum vestibulum dolor finibus id.
+          Pellentesque viverra justo eget lectus bibendum volutpat. Nam ac
+          mauris eu felis consectetur vestibulum. Donec ultricies cursus nisl a
+          volutpat. In lacinia egestas dui, et lacinia mauris. Vestibulum ante
+          ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+          curae; Morbi bibendum ipsum id purus malesuada fermentum. Aliquam
+          viverra dolor lacus, non ornare nulla suscipit sed. Suspendisse et
+          lacus a orci rutrum varius a ac odio.
+        </p>
+        <p>
+          Donec sed augue accumsan leo vehicula vulputate at egestas justo.
+          Pellentesque facilisis aliquam dapibus. Donec quis sem risus. Morbi
+          magna ex, sodales at libero quis, aliquet feugiat odio. Vivamus
+          euismod tristique orci vel sagittis. Quisque nibh libero, volutpat at
+          neque ac, mattis auctor dolor. Nulla non ante justo. Aenean odio
+          ligula, posuere tempor tempor vel, tempor nec tellus. Vestibulum
+          congue risus id libero dapibus, non auctor metus rhoncus. Integer et
+          sem sed leo sollicitudin consectetur eu non magna. Aliquam elit eros,
+          dictum non mauris ut, dictum vehicula mauris. Quisque in nulla
+          venenatis, faucibus arcu ac, molestie sem.
+        </p>
+        <p>
+          Integer massa augue, elementum vel gravida convallis, venenatis eu
+          odio. Pellentesque quis pellentesque nisl. Quisque elementum eros non
+          tincidunt rhoncus. In hac habitasse platea dictumst. Morbi id
+          convallis magna. Sed volutpat suscipit semper. Aenean aliquet dui in
+          mattis aliquet. Integer quis facilisis metus. Nam tempor at tortor
+          luctus lobortis. Aliquam sodales diam enim, non maximus sapien
+          tristique sit amet. Duis convallis sodales enim, in feugiat urna
+          rhoncus a.
+        </p>
       </div>
-    )
+    );
   }
 }
 
-export default About
+export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,30 @@ class Home extends Component {
       <div className="home">
         <h2>Welcome to Area 51</h2>
         <img src={area51Sign} alt="area 51 sign" />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed
+          ornare velit, in porta metus. Curabitur vel facilisis diam. Ut
+          tincidunt sodales nunc, rutrum vestibulum dolor finibus id.
+          Pellentesque viverra justo eget lectus bibendum volutpat. Nam ac
+          mauris eu felis consectetur vestibulum. Donec ultricies cursus nisl a
+          volutpat. In lacinia egestas dui, et lacinia mauris. Vestibulum ante
+          ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+          curae; Morbi bibendum ipsum id purus malesuada fermentum. Aliquam
+          viverra dolor lacus, non ornare nulla suscipit sed. Suspendisse et
+          lacus a orci rutrum varius a ac odio.
+        </p>
+        <p>
+          Donec sed augue accumsan leo vehicula vulputate at egestas justo.
+          Pellentesque facilisis aliquam dapibus. Donec quis sem risus. Morbi
+          magna ex, sodales at libero quis, aliquet feugiat odio. Vivamus
+          euismod tristique orci vel sagittis. Quisque nibh libero, volutpat at
+          neque ac, mattis auctor dolor. Nulla non ante justo. Aenean odio
+          ligula, posuere tempor tempor vel, tempor nec tellus. Vestibulum
+          congue risus id libero dapibus, non auctor metus rhoncus. Integer et
+          sem sed leo sollicitudin consectetur eu non magna. Aliquam elit eros,
+          dictum non mauris ut, dictum vehicula mauris. Quisque in nulla
+          venenatis, faucibus arcu ac, molestie sem.
+        </p>
       </div>
     );
   }
